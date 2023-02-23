9363e1af6b9a5376daf9f6c040225e7ceb99e043
FallenCap <fallencap18@gmail.com>
2023-02-23 20:22:40 +0530

    3rd Commit

--- src/components/layout/HeaderCartButton.jsx
+++ src/components/layout/HeaderCartButton.jsx
@@ -1,17 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from '../css/HeaderCartButton.module.css';
 import CartContext from '../../context/CartContext';
 import { BsFillCartFill } from 'react-icons/bs';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <BsFillCartFill />
       </span>
